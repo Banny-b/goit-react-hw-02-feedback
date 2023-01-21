7ba49c562c5921e2f0a8a05a044e85e7ad24d230
Sergey Petukhov <banny75@i.ua>
2023-01-21 13:08:06 +0200

    feedback
    hw-02-1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React from 'react';
+import Section from './Feedback/Sectons';
+import FeedbackButtons from './Feedback/Buttons';
+import Notification from './Feedback/Notification';
+import Statistics from './Feedback/Statistics';
+import css from './App.module.css';
+
+
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  keys = Object.keys(this.state);
+
+  countTotalFeedback = () => {
+    return Object.values(this.state).reduce((a, b) => a + b, 0);
+  };
+
+  countPositiveFeedbackPercentage = total => {
+    if (!this.state.good) {
+      return 0;
+    }
+    return Math.round((100 / total) * this.state.good);
+  };
+
+  click = event => {
+    const name = event.target.name;
+    this.setState({ [name]: this.state[name] + 1 });
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    const total = this.countTotalFeedback();
+    const positivePercentage = this.countPositiveFeedbackPercentage(total);
+    return (
+      <section className={css.section}>
+        <h1 className={css.visuallyHidden}>Feedback</h1>
+        <div className={css.box}>
+          <Section title="Please leave feedback">
+            <FeedbackButtons options={this.keys} onLeaveFeedback={this.click} />
+          </Section>
+          <Section title="Statistics">
+            {total === 0 ? (
+              <Notification message="There is no feedback" />
+            ) : (
+              <Statistics
+                good={good}
+                neutral={neutral}
+                bad={bad}
+                total={total}
+                positivePercentage={positivePercentage}
+              />
+            )}
+          </Section>
+        </div>
+      </section>
+    );
+  }
 };
